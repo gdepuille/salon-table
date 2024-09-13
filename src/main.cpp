5ded58e81284991a0fdd3d22d4f0e983f82e9e84
Gregory DEPUILLE <gregory.depuille@gmail.com>
2024-09-13 10:51:35 +0200

    Change screen lib

--- src/main.cpp
+++ src/main.cpp
@@ -1,32 +1,46 @@
 #include <Arduino.h>
+
 #include <I2CScanner.h>
 #include <FastLED.h>
+
+#include <U8g2lib.h>
+
+#ifdef U8X8_HAVE_HW_SPI
+#include <SPI.h>
+#endif
+#ifdef U8X8_HAVE_HW_I2C
+#include <Wire.h>
+#endif
+
 #include <Adafruit_CAP1188.h>
-#include <Adafruit_SSD1306.h>
+
+#if defined(DEBUG)
+#define F_CPU 16000000L
+#endif
 
 #if defined(ARDUINO_NANO_ESP32) or defined(ARDUINO_SAMD_NANO_33_IOT)
+#if defined(ARDUINO_SAMD_NANO_33_IOT)
+#include <WiFiNINA.h>
+#else
+#include <WiFi.h>
+#endif
 #include <ArduinoOTA.h>
 #endif
 I2CScanner i2cScan;
 
+// OLED
+U8G2_SSD1306_128X64_NONAME_F_HW_I2C u8g2(U8G2_R0, /* reset=*/ U8X8_PIN_NONE);
+
+// Capacitive sensor
+
+auto cap = Adafruit_CAP1188();
+
 // Leds
+#define LEDS_DATA_PIN 6
 #define LEDS_WIDHT 56
 #define LEDS_HEIGHT 19
-#define NUM_LEDS ((LEDS_WIDHT * 2) + (LEDS_HEIGHT * 2))
-#define DATA_PIN 6
+#define NUM_LEDS 3 // ((LEDS_WIDHT * 2) + (LEDS_HEIGHT * 2))
 CRGB leds[NUM_LEDS];
-
-// Screen
-#define SCREEN_WIDTH 128 // OLED display width, in pixels
-#define SCREEN_HEIGHT 64 // OLED display height, in pixels
-#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address; 0x3D for 128x64, 0x3C for 128x32
-auto display = Adafruit_SSD1306(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire);
-
-// Capacitive touch
-#define SENSITIVITY_REGISTER 0x1F
-#define SENSITIVITY_DELTA_SENSE 0x1
-#define SENSITIVITY_BASE_SHIFT 0xF
-auto cap = Adafruit_CAP1188();
 
 #if defined(ARDUINO_NANO_ESP32) or defined(ARDUINO_SAMD_NANO_33_IOT)
 #define WIFI_SSID "picotteland-ng"
@@ -34,52 +48,55 @@ auto cap = Adafruit_CAP1188();
 #define HOSTNAME "table-salon"
 #endif
 
+// IO pins
+#define BTN_UP 2
+#define BTN_DOWN 3
+#define BTN_ENTER 4
 
 void setup() {
 #if defined(DEBUG)
   // Configure Serial monitoring
+  delay(1000);
   Serial.begin(115200);
-  Serial.println("Table test!");
+  Serial.println("Start table salon !");
 #endif
 
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.clearDisplay();
-  display.display();
+#if defined(DEBUG)
+  Serial.println("I2C Configuration");
+#endif
+  i2cScan.Init();
+#if defined(DEBUG)
+  i2cScan.Scan();
+#endif
+
+#if defined(DEBUG)
+  Serial.println("Display Configuration");
+#endif
+
+  u8g2.begin();
+  u8g2.setFont(u8g2_font_ncenB08_tr);	// choose a suitable font
+
+  u8g2.clearBuffer();					// clear the internal memory
+  u8g2.drawStr(0, 10, "Table salon !");	// write something to the internal memory
+  u8g2.drawStr(0, 20, COMMIT_HASH);	// write something to the internal memory
+  u8g2.drawStr(0, 30, TIMESTAMP);	// write something to the internal memory
+  u8g2.sendBuffer();					// transfer internal memory to the display
   delay(1000);
 
 #if defined(ARDUINO_NANO_ESP32) or defined(ARDUINO_SAMD_NANO_33_IOT)
-
 #if defined(DEBUG)
   Serial.println("Connecting to WiFi");
 #endif
-
-  uint8_t idxDot = 0;
-  while(ArduinoOTA.status() != WL_CONNECTED) {
+  WiFiClass::setHostname(HOSTNAME);
+  while(WiFiClass::status() != WL_CONNECTED) {
+    WiFi.begin(WIFI_SSID, WIFI_PASS);
 #if defined(DEBUG)
     Serial.print(".");
 #endif
-    display.clearDisplay();
-    display.setCursor(0,0);
-    display.println("WiFi connecting");
-    for (uint8_t i = 0; i < idxDot; i++) {
-      display.print('.');
-      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-      delay(100);
-    }
-    display.display();
-
-    idxDot++;
-    if (idxDot > 3) {
-      idxDot = 0;
-    }
-    delay(1000);
+    delay(10000);
   }
-#endif
-  display.clearDisplay();
-  display.display();
-
 #if defined(DEBUG)
+  Serial.println();
   Serial.println("Wifi OK");
   Serial.print("SSID: ");
   Serial.println(WiFi.SSID());
@@ -96,90 +113,90 @@ void setup() {
   Serial.println(" dBm");
 #endif
 
-#if defined(ARDUINO_NANO_ESP32) or defined(ARDUINO_SAMD_NANO_33_IOT)
+
+#if defined(DEBUG)
+  Serial.println("Enable OTA flash upgrade");
+#endif
+  ArduinoOTA.setHostname(WiFiClass::getHostname());
+  ArduinoOTA.onStart([]() -> void {
+    Serial.println("Start OTA");
+    digitalWrite(LED_BUILTIN, LOW);
+    digitalWrite(LED_RED, LOW);
+    digitalWrite(LED_GREEN, LOW);
+    digitalWrite(LED_BLUE, LOW);
+  });
+  ArduinoOTA.onEnd([]() -> void {
+    Serial.println("End OTA");
+    digitalWrite(LED_BUILTIN, HIGH);
+    digitalWrite(LED_RED, HIGH);
+    digitalWrite(LED_GREEN, HIGH);
+    digitalWrite(LED_BLUE, HIGH);
+  });
+  ArduinoOTA.onProgress([](int current, int total) -> void {
+    const int pct = (current / (total / 100));
+    Serial.print("Upgrade ");
+    Serial.print(pct);
+    Serial.println("%");
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+
+    if (pct > 40) {
+      digitalWrite(LED_RED, HIGH);
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_BLUE, LOW);
+    }
+    if (pct > 60) {
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_GREEN, HIGH);
+      digitalWrite(LED_BLUE, LOW);
+    }
+    if (pct > 90) {
+      digitalWrite(LED_RED, LOW);
+      digitalWrite(LED_GREEN, LOW);
+      digitalWrite(LED_BLUE, HIGH);
+    }
+  });
+#if defined(ARDUINO_NANO_ESP32)
   ArduinoOTA.begin();
+#elif defined(ARDUINO_SAMD_NANO_33_IOT)
+  ArduinoOTA.begin(WiFi.localIP(), HOSTNAME, "password", InternalStorage);
+#endif
 #endif
 
+#if defined(DEBUG)
+  Serial.println("IO Configuration");
+#endif
+  pinMode(BTN_UP, INPUT_PULLUP);
+  pinMode(BTN_DOWN, INPUT_PULLUP);
+  pinMode(BTN_ENTER, INPUT_PULLUP);
   pinMode(LED_BUILTIN, OUTPUT);
 #if defined(ARDUINO_NANO_ESP32)
-  pinMode(LEDR, OUTPUT);
-  pinMode(LEDG, OUTPUT);
-  pinMode(LEDB, OUTPUT);
+  pinMode(LED_RED, OUTPUT);
+  pinMode(LED_GREEN, OUTPUT);
+  pinMode(LED_BLUE, OUTPUT);
 #endif
 
 
-  i2cScan.Init();
-  i2cScan.Scan();
-
-  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
-  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
 #if defined(DEBUG)
-    Serial.println(F("SSD1306 allocation failed"));
+  Serial.println("Led strip configuration");
 #endif
-
-    while (true) {
-      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-#if defined(ARDUINO_NANO_ESP32)
-      analogWrite(LEDR, digitalRead(LED_BUILTIN)? 0 : 255);
-#endif
-      delay(500);
-    }
-  }
-#if defined(DEBUG)
-  Serial.println(F("SSD1306 allocation success"));
-#endif
-
-  // Initialize the sensor, if using i2c you can pass in the i2c address
-  // if (!cap.begin(0x28)) {
-  if (!cap.begin()) {
-#if defined(DEBUG)
-    Serial.println("CAP1188 not found");
-#endif
-    while (true) {
-      digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-#if defined(ARDUINO_NANO_ESP32)
-      analogWrite(LEDR, digitalRead(LED_BUILTIN)? 0 : 255);
-#endif
-      delay(100);
-    }
-  }
-#if defined(DEBUG)
-  Serial.println("CAP1188 found!");
-#endif
-
-  cap.writeRegister(SENSITIVITY_REGISTER, (SENSITIVITY_DELTA_SENSE << 4) + SENSITIVITY_BASE_SHIFT);
-#if defined(DEBUG)
-  Serial.println("CAP1188 sensitivity changed!");
-#endif
-
-  CFastLED::addLeds<NEOPIXEL, DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
+  CFastLED::addLeds<NEOPIXEL, LEDS_DATA_PIN>(leds, NUM_LEDS);  // GRB ordering is assumed
   FastLED.setBrightness(20);
-
-  // Show initial display buffer contents on the screen --
-  // the library initializes this with an Adafruit splash screen.
-  display.display();
-  delay(2000); // Pause for 2 seconds
-  // Clear the buffer
-  display.clearDisplay();
 }
 
 uint8_t i = 0;
 
 void loop() {
-#if defined(ARDUINO_NANO_ESP32) or defined(ARDUINO_SAMD_NANO_33_IOT)
+#if defined(ARDUINO_NANO_ESP32)
   ArduinoOTA.handle();
+#elif defined(ARDUINO_SAMD_NANO_33_IOT)
+  ArduinoOTA.poll();
 #endif
 
-  uint8_t touched = cap.touched();
-  display.clearDisplay();
+  EVERY_N_MILLIS(200) {
+    digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
+  }
 
   EVERY_N_MILLIS(100) {
-    display.clearDisplay();
-    display.setCursor(0, 0);
-    display.print("Led : ");
-    display.println(i);
-    display.display();
-
     FastLED.clear();
     leds[i] = CRGB::Blue;
     i++;
